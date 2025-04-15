e85aa2e90cbbd22020821c0656fdb5f2ddb4b983
Júnior Stahl <juniorstahl@gmail.com>
2025-04-15 18:33:39 -0300

    alterações

--- src/Main.java
+++ src/Main.java
@@ -5,18 +5,47 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
-        Scanner sc = new Scanner (new File("C:\\Users\\Júnior\\trabalho_alest\\texto.txt"));
+        Scanner sc = new Scanner(new File("C:\\Users\\Júnior\\trabalho_alest\\texto.txt"));
 
+
+        StringBuilder texto = new StringBuilder();
         int contador = 0;
-        while(sc.hasNextLine()) {
+        while (sc.hasNextLine()) {
             String linha = sc.nextLine();
             String[] palavras = linha.split(" ");
 
-            for(String palavra : palavras) {
+            for (String words : palavras) {
+                for (int i = 0; i < words.length(); i++) {
+                    char c = words.charAt(i);
+                    if (c != '!' && c != '@' && c != '#' && c != '$' && c != '%' && c != '^' &&
+                            c != '&' && c != '*' && c != '(' && c != ')' && c != '-' && c != '_' &&
+                            c != '=' && c != '+' && c != '{' && c != '}' && c != '[' && c != ']' &&
+                            c != '|' && c != '\\' && c != ':' && c != ';' && c != '"' && c != '\'' &&
+                            c != '<' && c != '>' && c != ',' && c != '.' && c != '?' && c != '/' &&
+                            c != '~' && c != '`' && c!='á' && c!='ó' && c!='é') {
+                        texto.append(c);
+                    }
+                }
+                texto.append(" ");
                 contador++;
             }
         }
 
+        String resultado = texto.toString();
+        String [] palavrasSeparadas = resultado.split(" ");
+
+        for (String palavra : palavrasSeparadas) {
+            int contadorLocal = 0;
+            for (String s : palavrasSeparadas) {
+                if (s.equals(palavra)) {
+                    contadorLocal++;
+                }
+            }
+            System.out.println(palavra + ": " + contadorLocal);
+
+        }
+
         System.out.println("Quantidade de palavras no texto: " + contador);
+        System.out.println(resultado);
     }
 }
